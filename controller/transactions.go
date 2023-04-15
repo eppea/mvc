799c7666de15891c9103975dab194d955b0e878a
eppea <63557122+eppea@users.noreply.github.com>
2023-04-15 14:31:17 +0700

    testing

---
+++ controller/transactions.go
@@ -0,0 +1,112 @@
+package controller
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+
+	"MVC/model"
+	"MVC/view"
+
+	"github.com/labstack/echo/v4"
+)
+
+var db *sql.DB
+
+func GetAllTransactions(c echo.Context) error {
+	rows, err := db.Query("SELECT id, description, amount FROM transactions")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to get transactions"})
+	}
+	defer rows.Close()
+
+	transactions := []model.Transaction{}
+	for rows.Next() {
+		var transaction model.Transaction
+		err := rows.Scan(&transaction.ID, &transaction.Description, &transaction.Amount)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to get transactions"})
+		}
+		transactions = append(transactions, transaction)
+	}
+
+	return c.JSON(http.StatusOK, transactions)
+}
+
+func GetTransaction(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, view.Response{Message: "Invalid transaction ID"})
+	}
+
+	var transaction model.Transaction
+	err = db.QueryRow("SELECT id, description, amount FROM transactions WHERE id = ?", id).Scan(&transaction.ID, &transaction.Description, &transaction.Amount)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, view.Response{Message: "Transaction not found"})
+		}
+		return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to get transaction"})
+	}
+
+	return c.JSON(http.StatusOK, transaction)
+}
+
+func CreateTransaction(c echo.Context) error {
+	transaction := new(model.Transaction)
+	if err := c.Bind(transaction); err != nil {
+		return c.JSON(http.StatusBadRequest, view.Response{Message: "Invalid request payload"})
+	}
+
+	result, err := db.Exec("INSERT INTO transactions (description, amount) VALUES (?, ?)", transaction.Description, transaction.Amount)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to create transaction"})
+	}
+
+	id, _ := result.LastInsertId()
+	transaction.ID = int(id)
+
+	return c.JSON(http.StatusCreated, transaction)
+}
+
+func UpdateTransaction(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, view.Response{Message: "Invalid transaction ID"})
+	}
+
+	transaction := new(model.Transaction)
+	if err := c.Bind(transaction); err != nil {
+		return c.JSON(http.StatusBadRequest, view.Response{Message: "Invalid request payload"})
+	}
+
+	result, err := db.Exec("UPDATE transactions SET description = ?, amount = ? WHERE id = ?", transaction.Description, transaction.Amount, id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to update transaction"})
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		return c.JSON(http.StatusNotFound, view.Response{Message: "Transaction not found"})
+	}
+
+	return c.JSON(http.StatusOK, transaction)
+}
+
+func DeleteTransaction(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, view.Response{Message: "Invalid transaction ID"})
+	}
+
+	result, err := db.Exec("DELETE FROM transactions WHERE id = ?", id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, view.Response{Message: "Failed to delete transaction"})
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		return c.JSON(http.StatusNotFound, view.Response{Message: "Transaction not found"})
+	}
+
+	return c.JSON(http.StatusOK, view.Response{Message: "Transaction deleted"})
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"mvc/controller"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var db *sql.DB
+
+func main() {
+	var err error
+
+	// Koneksi ke database
+	db, err = sql.Open("mysql", "root:EANHHUFWsX2ocayI4WXW@tcp(containers-us-west-143.railway.app:6475)/railway")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Init Echo framework
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/transactions", controller.GetAllTransactions)
+	e.GET("/transactions/:id", controller.GetTransaction)
+	e.POST("/transactions", controller.CreateTransaction)
+	e.PUT("/transactions/:id", controller.UpdateTransaction)
+	e.DELETE("/transactions/:id", controller.DeleteTransaction)
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Start(fmt.Sprintf(":%s", port))
+}
